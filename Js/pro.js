2b2dd84cbd31a57dd31fcc56c13b233a8662a1b0
Karishma Kate <83213536+karishma502@users.noreply.github.com>
2021-06-13 11:31:57 +0530

    Update pro.js

--- Js/pro.js
+++ Js/pro.js
@@ -9,6 +9,7 @@ addBtn.addEventListener("click", function (e) {
 
 
     let addTxt = document.getElementById("addTxt");
+    let addTitle = document.getElementById("addTitle");
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         notesObj = [];
@@ -16,10 +17,14 @@ addBtn.addEventListener("click", function (e) {
     else {
         notesObj = JSON.parse(notes);
     }
-
-    notesObj.push(addTxt.value);
+    let myObj = {
+        title:addTitle.value,
+        text:addTxt.value
+    }
+    notesObj.push(myObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     addTxt.value = "";
+    addTitle.value = "";
     console.log(notesObj);
 
     showNotes();
@@ -40,8 +45,8 @@ function showNotes() {
         html += `
             <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">Note ${index + 1}</h5>
-                    <p class="card-text">${element}</p>
+                    <h5 class="card-title">${element.title}</h5>
+                    <p class="card-text">${element.text}</p>
                     <button id="${index}"  onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                 </div>
             </div> `;
